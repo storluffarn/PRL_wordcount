24b42fdaded790622e1031269c28ea108b6ac5ec
David Andersson <storluffarn@gmail.com>
2019-10-14 15:40:54 +0200

    added automatic word count from source tex, rudimentary multi-row side by side image support, and a more thorough introduction

--- prlpictowords.py
+++ prlpictowords.py
@@ -1,57 +1,98 @@
 #
 # Script for transforming figures to number of words in prl
 #
-# Pretty ad hoc at the moment as it assumes all images are either 1. single 
-# images of one column width, or 2. forms pairs of two images to form two
-# column image. At the moment the script does not know which images will
-# combine to a two column image, so the final value will be an estimate
+# written by: David Andersson
+#
+# Limitations:
+# Pretty ad hoc, but it does the trick mostly. The program only handles either single
+# or two side by side images at the moment. However, more complex figures can be 
+# estimated by the offset parameter. Side by side images are assumed to have the same
+# aspect ratio. The automated word counting from tex source only works on UNIX sytems
+# with texcount installed (tested for version 3.1). Probably doesn't work for obscure
+# image formats, see Python PIL package
 #
 # Usage: 
-# * put file where your images are at
-# * put number of word in nwords variable
-# * either put your image names in "lookfor" or uncomment "ifendswith()" to 
+# * put the file where your images are at
+# * put your tex source file in "texfile" or put your current number of word in 
+#   "nwords" and comment out the lines before that
+# * either put your file names in "lookfor" or uncomment "ifendswith(*.type)" to 
 #   automatiacally append all images of some file type
+# * for side by side images: only add only *one* of the images to "lookfor", 
+#   and add those files' filenames to the "sidebyside" list. e.g. if you have two
+#   images img1.x and img2.x that you wand side by side, only add img1 to "lookfor",
+#   but also add it to "sidebyside". for multi column images, only add the first of
+#   each column
+# * word "balance" holds the remaining PRL word count, it is printed in the end, 
+#   you want it be > 0 in order not to be in trouble
 #
+# Changelog:
+# 
 # TODO 
+#
+# * more image layouts than only side by side with two columns
+# * possibly read in from pdf instead of tex
+# * update to python 3
 #
 
 
 from PIL import Image
 import os
+import subprocess
 
-nwords = 2750
+texfile = "paper.tex"
+child = subprocess.Popen('texcount {} | grep "Words in text"'.format(texfile),stdout=subprocess.PIPE,shell=True)
+wordsstring = child.communicate()[0]
+words = wordsstring.split()[-1]
+
+nwords = int(words)
+
+print "number of words in source text {}".format(words)
 
 filenames = []
 sizes = []
 aspects = []
 
-lookfor = ["model.png","minmaxplot.png","fstd.png","pstd.png","thermal.png","irregular.png","trajectory1.png","trajectory2.png","doubleslip.png","offset.png","veldep.png","veldeppot.png"]
+lookfor = ["file1.png","file2.png"]
 
-for filename in os.listdir("./"):
-#    if filename.endswith(".png"):
+sidebyside = ["file1.png"]
+
+print "processing image:"
+for filename in os.listdir("./"):        # read in all files in lookfor
+#    if filename.endswith(".png"):      # read in all files of *.type
     if filename in lookfor:
         print filename
         filenames.append(filename)
         img = Image.open(filename)
-        sizes.append(img.size)
+
+        x,y = img.size
+        if filename in sidebyside:
+            x = x*2
+
+        size = [x,y]
+
+        sizes.append(size)
     else:
         continue
 
-for el in sizes:
+print "fetching image sizes"
+for el in sizes:                        # claculate all aspects
     print [el[0],el[1]]
     aspect = float(el[0])/float(el[1])
     aspects.append(aspect)
 
-#print (aspects)
+print "the calculated the aspects"
+print (aspects)
 
 total = 0;
 for el in aspects:
     total += 300/(0.5*el) + 40
     
-total /= 2
+# offset = 2                            # used for estimating non-standard image configurations
+# total /= offset
 estimate = total + nwords
 balance = 3750 - estimate
 
 print "estimated word count from figures: {}".format(total)
 print "estimated total word count: {}".format(estimate)
 print "your estimated word count balance is: {}".format(balance)
+
